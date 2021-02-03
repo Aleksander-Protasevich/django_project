2ee535b9df9fe71097831d5ce211e0a06f5a5990
Aleksander Protasevich <aprotasevitch@gmail.com>
2021-02-03 23:00:32 +0300

    html template done

--- src/directory/views.py
+++ src/directory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+from directory.models import Author
 
 # Create your views here.
+
+def home_page(request):
+    auth = Author.objects.last()
+    context = {"auth":auth}
+    return render(request, template_name="home.html", context=context)
